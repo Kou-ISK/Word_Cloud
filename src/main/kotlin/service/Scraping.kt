c01036e27eabb1ba9c36be2b6e43d37b6d5c0be4
k-isaka <isk.rugby@gmail.com>
2023-03-11 00:00:59 +0900

    feat: 「記事をもっと見る」ボタンをクリックする処理を追加(Tryを追加)

--- src/main/kotlin/service/Scraping.kt
+++ src/main/kotlin/service/Scraping.kt
@@ -4,7 +4,6 @@ import org.openqa.selenium.By
 import org.openqa.selenium.JavascriptExecutor
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
-import java.io.BufferedWriter
 import java.io.File
 
 
@@ -22,32 +21,35 @@ class Scraping {
         driver.get("https://note.com/kou_isk")
         // TODO 「記事をもっと見る」ボタンをクリックできるようにする
         Thread.sleep(1000)
-
-        Thread.sleep(1000)
         loadMoreContent()
         var noteList = driver.findElement(By.className("o-noteList"))
             .findElements(By.className("m-largeNoteWrapper__link"))
         println(noteList.size)
         var text = ""
+        val file = File("/Users/isakakou/Desktop/名称未設定フォルダ/note_text.txt")
         // TODO スクレイピングのメソッドが異常終了しないように修正する
         for (i in 0..noteList.size) {
             driver.get("https://note.com/kou_isk")
-            Thread.sleep(1000)
+            Thread.sleep(2000)
             loadMoreContent()
+            println(i)
             noteList = driver.findElement(By.className("o-noteList"))
                 .findElements(By.className("m-largeNoteWrapper__link"))
-            Thread.sleep(3000)
+            Thread.sleep(2000)
             noteList[i].click()
             println("==============================")
-            Thread.sleep(3000)
-            driver.findElement(By.className("note-common-styles__textnote-body")).findElements(By.tagName("p"))
-                .forEach { p ->
-                    text += p.text.trimIndent().trim().replace("\\n", "")
-                    println(p.text.trimIndent().trim().replace("\\n", ""))
-                }
-            Thread.sleep(3000)
-            val file = File("/Users/isakakou/Desktop/名称未設定フォルダ/note_text.txt")
-            file.writeText(text, Charsets.UTF_8)
+            Thread.sleep(2000)
+            try {
+                driver.findElement(By.className("note-common-styles__textnote-body")).findElements(By.tagName("p"))
+                    .forEach { p ->
+                        text += p.text.trimIndent().trim().replace("\\n", "")
+                        println(p.text.trimIndent().trim().replace("\\n", ""))
+                    }
+                Thread.sleep(2000)
+                file.writeText(text, Charsets.UTF_8)
+            } catch (e: Exception) {
+                println(e)
+            }
         }
 
         // ブラウザを閉じる
