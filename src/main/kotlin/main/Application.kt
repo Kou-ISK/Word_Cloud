331998733636aa956d50d44b594a5cdad791e967
k-isaka <isk.rugby@gmail.com>
2023-03-16 00:34:52 +0900

    feat: 引数ありで実行時の挙動を設定 TODO 成功時と失敗時の差分を調査する

--- src/main/kotlin/main/Application.kt
+++ src/main/kotlin/main/Application.kt
@@ -6,12 +6,12 @@ import service.Scraping
 import service.WordCloud
 import java.io.File
 
-fun main(args: String) {
+fun main(args: Array<String>) {
     //val inputText = Scraping().getTextFromMyNote()
     val inputText =
         File(INPUT_FILE_PATH).readText().trimIndent().trim().replace("\\n", "")
     val analyze = Analyze(inputText)
-    println(analyze.getNoun2())
+    //println(analyze.getThisPart(args.get(0)))
     println(analyze.getAssociation())
     val frequencyMap = analyze.countFrequency(analyze.getNoun())
     WordCloud(frequencyMap).draw()
